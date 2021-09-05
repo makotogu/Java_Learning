35a038d4770ecc01d731fc965d003d655b24984f
makotogu <jianwangp@icloud.com>
2021-09-05 10:02:34 +0800

    符号表

---
+++ 数据结构/algorithm/src/com/makotogu/algorithm/linear/SymbolTable.java
@@ -0,0 +1,62 @@
+package com.makotogu.algorithm.linear;
+
+public class SymbolTable<K, V> {
+    private Node head;
+    private int N;
+
+    private class Node {
+        public K key;
+        public V value;
+        public Node next;
+
+        public Node(K key, V value, Node next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+    public SymbolTable() {
+        this.head = new Node(null, null, null);
+        this.N = 0;
+    }
+
+    public int size() {
+        return N;
+    }
+
+    public void put(K key, V value) {
+        Node node = head;
+        while (node.next != null) {
+            node = node.next;
+            if (node.key.equals(key)) {
+                node.value = value;
+                return;
+            }
+        }
+        Node newNode = new Node(key, value, null);
+        node.next = newNode;
+        N++;
+    }
+    public void delete(K key) {
+        Node node = head;
+        while (node.next != null) {
+            if (node.next.key.equals(key)) {
+                node.next = node.next.next;
+                N--;
+                return;
+            }
+            node = node.next;
+        }
+    }
+    public V get(K key){
+        Node node = head;
+        while (node.next != null) {
+            node = node.next;
+            if (node.key.equals(key)) {
+                return node.value;
+            }
+        }
+        return null;
+    }
+}
---
+++ 数据结构/algorithm/src/com/makotogu/algorithm/test/SymbolTableTest.java
@@ -0,0 +1,15 @@
+package com.makotogu.algorithm.test;
+
+import com.makotogu.algorithm.linear.SymbolTable;
+
+public class SymbolTableTest {
+    public static void main(String[] args) {
+        SymbolTable<Integer, String> symbolTable = new SymbolTable<Integer, String>();
+        symbolTable.put(1,"小王");
+        symbolTable.put(2,"老王");
+        System.out.println(symbolTable.size());
+        symbolTable.delete(2);
+        symbolTable.put(1, "老王八");
+        System.out.println(symbolTable.get(1));
+    }
+}
